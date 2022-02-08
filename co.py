4fc0dc646e7d7da491d28f50eb17aaf958038546
gowthamkowshik <86563133+gowthamkowshik@users.noreply.github.com>
2022-02-08 20:34:00 +0530

    Create co.py

---
+++ co.py
@@ -0,0 +1,9 @@
+#comparison operators
+a=int(input("Enter tha a value :"))
+b=int(input("Enter the b value :"))
+print("a is greaterthen b is ",a>b)
+print("a is lessthen b is ",a<b)
+print("a is greaterthen  or equal to b is ",a>=b)
+print("a is lessthen or equal to b is ",a<=b)
+print("a is equal to b is ",a==b)
+print("a is not equal to b is ",a!=b)
