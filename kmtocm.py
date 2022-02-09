19c83ef5b260c0869209aa492859fbd065f9d06f
gowthamkowshik <86563133+gowthamkowshik@users.noreply.github.com>
2022-02-09 06:18:16 +0530

    Update kmtocm.py

--- kmtocm.py
+++ kmtocm.py
@@ -1,4 +1,4 @@
 # program for converting kilometers into centimeters
 n=int(input("Enter the n numer of kilometers :"))
 cm=n*100000
-print(n,"kilometers"= cm "centimeters")
+print(n,"kilometers" = cm "centimeters")
