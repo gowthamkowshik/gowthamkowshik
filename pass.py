ccca35518c861cefaa757be75924fc02fd7d57b2
gowthamkowshik <86563133+gowthamkowshik@users.noreply.github.com>
2022-02-10 18:59:19 +0530

    Create pass.py

---
+++ pass.py
@@ -0,0 +1,11 @@
+english = float(input(" Please enter English Marks: "))
+math = float(input(" Please enter Math score: "))
+computers = float(input(" Please enter Computer Marks: "))
+physics = float(input(" Please enter Physics Marks: "))
+chemistry = float(input(" Please enter Chemistry Marks: "))
+
+total = english + math + computers + physics + chemistry
+percentage = (total / 500) * 100
+
+print("Total Marks = %.2f"  %total)
+print("Marks Percentage = %.2f"  %percentage)
