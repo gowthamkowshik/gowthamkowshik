4ffe1f9d79013a1d8d4c959a214b3e7862ab1f14
gowthamkowshik <86563133+gowthamkowshik@users.noreply.github.com>
2022-02-12 20:13:40 +0530

    Create num.py

---
+++ num.py
@@ -0,0 +1,10 @@
+x=float (input ("enter any value"))
+y=float (input ("enter any value"))
+if x>y:
+  print ("1st value is big")
+else:
+  Print ("2nd value is big")
+if x==y:
+  Print ("two values are equal!!")
+else:
+   Print ("two numbers are not equal")
