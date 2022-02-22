4f8452f6e4813fc8a4a47443a3870e3c5c47502c
gowthamkowshik <86563133+gowthamkowshik@users.noreply.github.com>
2022-02-22 21:29:21 +0530

    Create star.py

---
+++ star.py
@@ -0,0 +1,11 @@
+n = int (input())
+for i in range (1, n+1):
+for k in range (n-1,0,-1): print(" ", end=" ")
+for j in range (1,i+1):
+print ("*", end="") print("\n")
+for i in range (1, n+1):
+for k in range (1, 1):
+print("",end=" ")
+for i in range (n-i+1, 0,-1):
+print("",end=" ") 
+print ("\n")
