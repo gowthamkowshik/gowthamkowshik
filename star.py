1211140977fbe07f9a0360054ff0b3d5e8763f7f
gowthamkowshik <86563133+gowthamkowshik@users.noreply.github.com>
2022-02-22 21:57:02 +0530

    Update star.py

--- star.py
+++ star.py
@@ -1,11 +1,10 @@
-n = int (input())
-for i in range (1, n+1):
-for k in range (n-1,0,-1): print(" ", end=" ")
-for j in range (1,i+1):
-print ("*", end="") print("\n")
-for i in range (1, n+1):
-for k in range (1, 1):
-print("",end=" ")
-for i in range (n-i+1, 0,-1):
-print("",end=" ") 
-print ("\n")
+n = int(input())
+for i in range(1,n+1):
+  for j in range(1,i+1):
+   print(j,end=" ")
+    i=i+1
+    print("\n")
+for i in range(n+1,0,-1):
+  for j in range(1,i+1):
+   print(j,end=" ")
+   print("\n")
