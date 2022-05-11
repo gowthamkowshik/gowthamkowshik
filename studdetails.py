421942da390b63e3bf72be3fa7ebc46239472377
gowthamkowshik <86563133+gowthamkowshik@users.noreply.github.com>
2022-05-11 07:31:21 +0530

    Create studdetails.py

---
+++ studdetails.py
@@ -0,0 +1,9 @@
+student_details = {1:["Gowtham",'BCA',980],
+                2:["Kowshik",'MPCS',964],
+                3:["Thanisha",'MECS',683],
+                4:["Manideep",'BBA',466]} 
+print ("The dictionary before performing remove is:", 
+(student_details))
+del student_details[4] 
+print ("The dictionary after remove is :",
+(student_details))
